edc64a8993e34d9a544c837a28e886540ed1ee74
StewartPedroza <124059158+StewartPedroza@users.noreply.github.com>
2023-02-28 22:59:21 +0800

    Add files via upload

---
+++ Pedroza_MissU/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Pedroza_MissU
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.top20 = new System.Windows.Forms.ListBox();
+            this.top10 = new System.Windows.Forms.ListBox();
+            this.top3 = new System.Windows.Forms.ListBox();
+            this.MissUniverse = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.upload = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // top20
+            // 
+            this.top20.FormattingEnabled = true;
+            this.top20.Location = new System.Drawing.Point(41, 32);
+            this.top20.Name = "top20";
+            this.top20.Size = new System.Drawing.Size(159, 303);
+            this.top20.TabIndex = 0;
+            this.top20.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // top10
+            // 
+            this.top10.FormattingEnabled = true;
+            this.top10.Location = new System.Drawing.Point(248, 32);
+            this.top10.Name = "top10";
+            this.top10.Size = new System.Drawing.Size(159, 303);
+            this.top10.TabIndex = 1;
+            this.top10.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // top3
+            // 
+            this.top3.FormattingEnabled = true;
+            this.top3.Location = new System.Drawing.Point(460, 32);
+            this.top3.Name = "top3";
+            this.top3.Size = new System.Drawing.Size(159, 303);
+            this.top3.TabIndex = 2;
+            this.top3.SelectedIndexChanged += new System.EventHandler(this.listBox3_SelectedIndexChanged);
+            // 
+            // MissUniverse
+            // 
+            this.MissUniverse.FormattingEnabled = true;
+            this.MissUniverse.Location = new System.Drawing.Point(688, 32);
+            this.MissUniverse.Name = "MissUniverse";
+            this.MissUniverse.Size = new System.Drawing.Size(159, 303);
+            this.MissUniverse.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(294, 363);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Print top 10";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(503, 363);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Print top 3";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(707, 363);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(131, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Miss Universe";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // upload
+            // 
+            this.upload.Location = new System.Drawing.Point(78, 363);
+            this.upload.Name = "upload";
+            this.upload.Size = new System.Drawing.Size(75, 23);
+            this.upload.TabIndex = 7;
+            this.upload.Text = "Upload";
+            this.upload.UseVisualStyleBackColor = true;
+            this.upload.Click += new System.EventHandler(this.upload_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(929, 450);
+            this.Controls.Add(this.upload);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.MissUniverse);
+            this.Controls.Add(this.top3);
+            this.Controls.Add(this.top10);
+            this.Controls.Add(this.top20);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox top20;
+        private System.Windows.Forms.ListBox top10;
+        private System.Windows.Forms.ListBox top3;
+        private System.Windows.Forms.ListBox MissUniverse;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button upload;
+    }
+}
+
---
+++ Pedroza_MissU/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pedroza_MissU
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (top10.SelectedItem != null)
+            {
+                string selectedItem = top10.SelectedItem.ToString();
+                top10.Items.Remove(selectedItem);
+                top3.Items.Add(selectedItem);
+
+                if (top3.Items.Count == 3)
+                {
+                    MessageBox.Show("Top 3 is full.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+            }
+
+            if (top3.Items.Count == 3)
+            {
+                top10.Enabled = false;
+            }
+        }
+
+        private void listBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (top3.SelectedItem != null)
+            {
+
+                string selectedItem = top3.SelectedItem.ToString();
+                top3.Items.Remove(selectedItem);
+                MissUniverse.Items.Add(selectedItem);
+
+                MessageBox.Show(selectedItem + " is the Miss Universe!", "Congratulations!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            if (MissUniverse.Items.Count == 1)
+            {
+                top3.Enabled = false;
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (top20.SelectedItem != null)
+            {
+
+                string selectedItem = top20.SelectedItem.ToString();
+
+                if (top10.Items.Count == 10)
+                {
+                    MessageBox.Show("Top 10 is full.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+
+
+                top20.Items.Remove(selectedItem);
+                top10.Items.Add(selectedItem);
+            }
+
+            if (top10.Items.Count == 10)
+            {
+                top20.Enabled = false;
+            }
+
+
+        }
+
+        private void upload_Click(object sender, EventArgs e)
+        {
+            using (StreamReader sr = new StreamReader(@"C:\Users\johnp\OneDrive\Desktop\top20.txt"))
+                {
+                    string line;
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    top20.Items.Add(line);
+                }
+            }
+        }
+        private void SaveFile(ListBox listBox, string fileName)
+        {
+            using (StreamWriter sw = new StreamWriter(fileName))
+            {
+                foreach (string item in listBox.Items)
+                {
+                    sw.WriteLine(item);
+                }
+            }
+        }
+
+            private void button1_Click(object sender, EventArgs e)
+        {
+            if (top10.Items.Count != 0)
+            {
+                string fileName = "Top10.txt";
+
+                SaveFile(top10, fileName);
+
+                MessageBox.Show("Success!", "Saved", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("The list is empty. Nothing to print.");
+                return;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (top3.Items.Count != 0)
+            {
+                string fileName = "Top3.txt";
+
+                SaveFile(top3, fileName);
+
+                MessageBox.Show("Success!", "Saved", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("The list is empty. Nothing to print.", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (MissUniverse.Items.Count != 0)
+            {
+                string fileName = "MissUniverse.txt";
+
+                SaveFile(MissUniverse, fileName);
+    
+                MessageBox.Show("Success!", "Saved", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("The list is empty. Nothing to print.");
+                return;
+            }
+        }
+    }
+}
